dc3b76d96111e39e3e2f6c3f1b16f07bd344e513
zero-jeden <tjbemben@gmail.com>
2024-10-21 22:46:54 -0400

    added decode function
    the second test case in the assignment shows if the digit + 3 goes to 10 or above the function should only take the number in the 10's place. I'll let you know on slack too.

--- decoder.py
+++ decoder.py
@@ -5,6 +5,25 @@ def encode(data):
     for char in data:
         out += str(int(char) + 3)
     return out
+
+
+#Tom Bemben decode function
+def decode(data):
+    result = ''
+    temp_array = []
+
+    for i in range(len(data)): #splits string into integers subtracts 3
+        temp_array.append(int(data[i] - 3))
+
+
+    for j in range(len(temp_array)): #creates string character following rollover digit rules
+        if temp_array[j] < 0:
+            result += str(temp_array[j] + 10)
+        else:
+            result += str(temp_array[j])
+
+
+
 if __name__ == "__main__":
     while(True):
         # menu
